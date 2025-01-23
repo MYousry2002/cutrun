fdda0f0b2f1b2773cc22c0c1138db9719a255d0c
MYousry2002 <me1117@profile-s1>
2025-01-23 15:17:33 -0500

    edit pileline code

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -9,11 +9,11 @@
 
 
 # Set the working directory
-cd ~/cut_run
+cd ~/cutrun
 
 # Activate conda environment
 source /home/me1117/anaconda3/etc/profile.d/conda.sh
 conda activate /home/me1117/anaconda3/envs/nextflow_env
 
 # Run the pipeline
-nextflow run nf-core/cutandrun -profile docker --input ./samplesheet.csv --genome mm10 --outdir ./results  --replicate_threshold 3  --normalisation_mode CPM --peakcaller SEACR,MACS2 -resume
+nextflow run nf-core/cutandrun -profile docker --input ./samplesheet.csv --genome mm10 --outdir ./results  --replicate_threshold 3  --normalisation_mode CPM --peakcaller MACS2 -resume
